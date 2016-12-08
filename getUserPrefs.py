ba5efd4530c21f1d772890897dc2654dacc78bf0
spherulitic <chris.lipe@gmail.com>
2016-12-08 00:31:05 -0600

    Worked on user prefs screen

--- getUserPrefs.py
+++ getUserPrefs.py
@@ -12,8 +12,10 @@ error = {"status": "success"}
 
 try:
   result = xl.getAllPrefs(userid)
-except:
-  error["status"] = "Error retrieving prefs"
+except Exception as ex:
+  template = "An exception of type {0} occured. Arguments:\n{1!r}"
+  message = template.format(type(ex).__name__, ex.args)
+  error["status"] = message
 
 print "Content-type: application/json\n\n"
 print json.dumps([result, error])
--- userPrefs.js
+++ userPrefs.js
@@ -8,69 +8,36 @@ function initUserPrefs() {
          success: showUserPrefs,
          error: function(jqXHR, textStatus, errorThrown) {
            console.log("Error retrieving user prefs. Status: " + textStatus + "  Error: " + errorThrown); }
-        {);
+});
+
 }
 
 function showUserPrefs(response, responseStatus) {
 
   // get response from ajax call
   // need prefs and currently occupied cardboxes
-
+  console.log("Prefs");
+  console.log(response[0]);
+  prefs = response[0]
   var prefForm = "<h3>User Preferences</h3>";
   prefForm += "<p>Xerafin will give you words that are due in order from cardbox 0 ";
-  prefForm += "through cardbox <select id='closetInput'></select>.<br>"
-  prefForm += "Then it will give you words scheduled in the near future. For each hour you work ahead, it will give you 10 words which are overdue in cardbox <span id=closetValue></span> or higher.<br>";
-  prefForm += "After <input type=text id='reschedHrsInput'></input> hours, it will add <input type=text id='newWordsAtOnceInput'></input> words for each additional hour you work ahead, as long as Cardbox 0 has fewer than <input type=text id='cb0SizeInput'> cards in it."
-  upForm += "<input type=file id='cboxFile' accept='.db'>";
-  upForm += "<input type=button value='Upload' id='uploadButton' onclick='uploadCardbox()'>";
-  if (URLExists('cardboxes/' + userid + '.db')) {
-    upForm += "<br><br><a href='cardboxes/" + userid + ".db'>Download Cardbox Here</a> <br> (Right Click and Save As...)";
-  upForm += "<hr>";
-  upForm += "<h3>Upload Custom Word List</h3>";
-  upForm += "<p>Upload a .txt file with one alphagram or word per line.</p>";
-  upForm += "<p>These words will be added to you cardbox ahead of the default study list.</p>";
-  upForm += "<input type=file id='wlistFile' accept='.txt'>";
-  upForm += "<input type=button value='Submit' id='listUploadButton' onclick='uploadNewWordList()'>";
-        }
-  $( "#gameArea" ).html(upForm);
+  prefForm += "through cardbox <select id='closetInput'>"
+  for (var x=6;x<20;x++) {
+    prefForm += "<option value='"+ x + "'";
+    if (x == parseInt(prefs.closet))
+      prefForm += " selected";
+    prefForm += ">"+ x +"</option>"; }
+  prefForm += "</select>.<br><br>"
+       nextCloset = prefs.closet + 1;
+  prefForm += "Then it will give you words scheduled in the near future. For each hour you work ahead, it will give you 10 words which are overdue in cardbox <span id=closetValue>" + nextCloset + "</span> or higher.<br><br>";
+  prefForm += "After <input type=text id='reschedHrsInput' maxlength=3 value='"+prefs.reschedHrs+"'></input> hours,"
+  prefForm += " it will add <input type=text id='newWordsAtOnceInput' maxlength=3 value='"+prefs.newWordsAtOnce+"'></input>";
+  prefForm += " words for each additional hour you work ahead, as long as Cardbox 0 has fewer than <input type=text id='cb0maxInput' maxlength=3 value='"+prefs.cb0max+"'> cards in it."
+  $( "#gameArea" ).html(prefForm);
 
   }
 
-function uploadCardbox() {
-  $( "#uploadButton" ).prop("disabled", true);
-  $( "#uploadButton" ).val("Uploading...");
-  var file = document.getElementById('cboxFile').files[0];
-  var formdata = new FormData();
-  formdata.append(userid, file, 'cardbox.db');
-  $.ajax({
-	type: 'POST',
-	url: 'uploadCardbox.py',
-	data: formdata,
-	processData: false,
-	contentType: false,
- 	success: cardboxUploadCallback});
-}
+function setPrefs() {
 
-function cardboxUploadCallback(response, responseStatus) {
-  $( "#uploadButton" ).prop("disabled", false);
-  $( "#uploadButton" ).val("Upload");
-  console.log("Cardbox upload script status is " + response.status);
-}
-
-function uploadNewWordList() {
-  $("#listUploadButton").prop("disabled", true);
-  $("#listUploadButton").val("Uploading...");
-  var file = document.getElementById('wlistFile').files[0];
-  var formdata = new FormData();
-  formdata.append(userid, file, 'list.txt');
-  $.ajax({ type: 'POST',
-	   url: 'uploadNewWordList.py', 
-          data: formdata,
-          processData: false,
-          contentType: false,
-          success: function(response, responseStatus) {
-		$('#listUploadButton').prop('disabled', false);
-                $('#listUploadButton').val('Success');
-          console.log("Word List Upload Status: " + response); }});
 }
 
