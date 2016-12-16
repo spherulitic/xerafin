0a34c7bfbdb463d18bee18de62765af8984a532a
spherulitic <chris.lipe@gmail.com>
2016-12-16 14:52:56 -0600

    Created xerafinSetup module to call

--- updateActive.py
+++ updateActive.py
@@ -2,17 +2,19 @@
 
 import MySQLdb as mysql
 import time
+import xerafinSetup as xs
 
-def updateActive (userid) :
-  now = int(time.time())
-  with mysql.connect("localhost", "slipkin_clipe", "xev1ous#", "slipkin_xerafin") as con: 
+def updateActive (userid, timestamp=None) :
+  if timestamp is None:
+    timestamp = int(time.time())
+  with xs.getMysqlCon() as con:
     if con is None:
       result["status"] = "Chat Database Connection Failed"
     else:
       try:
         command = "update login set last_active = %s where userid = %s"
         if (userid != 0):  # 0 is the system user
-          con.execute(command, (now, userid))
+          con.execute(command, (timestamp, userid))
       except mysql.Error, e:
          result["status"] = "MySQL error %d %s " % (e.args[0], e.args[1])
 
